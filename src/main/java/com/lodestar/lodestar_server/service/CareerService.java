630c6e7c7f6a05934401a90967a970c660baf63c
preferrrr <zzangsh2946@naver.com>
2023-08-29 16:03:33 +0900

    perf : 커리어 저장 jdbc를 사용해서 성능 개선

--- src/main/java/com/lodestar/lodestar_server/service/CareerService.java
+++ src/main/java/com/lodestar/lodestar_server/service/CareerService.java
@@ -6,6 +6,7 @@ import com.lodestar.lodestar_server.entity.Career;
 import com.lodestar.lodestar_server.entity.User;
 import com.lodestar.lodestar_server.exception.DuplicateCareerException;
 import com.lodestar.lodestar_server.repository.CareerRepository;
+import com.lodestar.lodestar_server.repository.CareerRepositoryJdbc;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,29 +20,32 @@ import java.util.List;
 public class CareerService {
 
     private final CareerRepository careerRepository;
+    private final CareerRepositoryJdbc careerRepositoryJdbc;
 
     public void saveCareer(User user, CareerDtos careerRequestDto) {
 
         if(careerRepository.existsByUser(user))
             throw new DuplicateCareerException("userId : " + user.getId());
 
-        List<Career> careerList = new ArrayList<>();
-
-        for(CareerDto dto : careerRequestDto.getArr()) {
-            Career career = Career.builder()
-                    .user(user)
-                    .x(dto.getX())
-                    .y1(dto.getY().get(0))
-                    .y2(dto.getY().get(1))
-                    .rangeName(dto.getRangeName())
-                    .build();
-            careerList.add(career);
-        }
-
-        careerRepository.saveAll(careerList);
+//        List<Career> careerList = new ArrayList<>();
+//
+//        for(CareerDto dto : careerRequestDto.getArr()) {
+//            Career career = Career.builder()
+//                    .user(user)
+//                    .x(dto.getX())
+//                    .y1(dto.getY().get(0))
+//                    .y2(dto.getY().get(1))
+//                    .rangeName(dto.getRangeName())
+//                    .build();
+//            careerList.add(career);
+//        }
+//
+//        careerRepository.saveAll(careerList);
+        careerRepositoryJdbc.saveCareers(user.getId(), careerRequestDto.getArr());
 
     }
 
+    @Transactional(readOnly = true)
     public List<CareerDto> getCareer(User user) {
 
         List<CareerDto> careerDtos = new ArrayList<>();
@@ -57,7 +61,6 @@ public class CareerService {
     }
 
 
-    @Transactional(readOnly = true)
     public void modifyCareer(User user, CareerDtos careerDtos) {
 
         List<Career> careers = careerRepository.findCareersByUser(user); // 원래 저장되어 있던 것들
@@ -67,18 +70,11 @@ public class CareerService {
             rangeNames1.add(career.getRangeName()); // 추가된 커리어를 저장햐기 위함
         }
 
-        List<Career> addCareers = new ArrayList<>();
+        List<CareerDto> addCareers = new ArrayList<>();
 
         for(CareerDto careerDto : careerDtos.getArr()) { //추가된 커리어를 저장하기 위함
             if(!rangeNames1.contains(careerDto.getRangeName())) {
-                Career career = Career.builder()
-                        .user(user)
-                        .x(careerDto.getX())
-                        .y1(careerDto.getY().get(0))
-                        .y2(careerDto.getY().get(1))
-                        .rangeName(careerDto.getRangeName())
-                        .build();
-                addCareers.add(career);
+                addCareers.add(careerDto);
             }
         }
 
@@ -94,7 +90,7 @@ public class CareerService {
                 deleteCareers.add(career);
         }
 
-        careerRepository.saveAll(addCareers);
+        careerRepositoryJdbc.saveCareers(user.getId(), addCareers);
         careerRepository.deleteAllInBatch(deleteCareers);
 
     }

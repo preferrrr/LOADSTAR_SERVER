bfb56c6c37d241160044bc144795852505111263
preferrrr <zzangsh2946@naver.com>
2024-02-19 20:59:38 +0900

    refactor : SaveCareer jdbc 인자 Career 엔티티로 변경

--- src/main/java/com/lodestar/lodestar_server/career/repository/CareerRepositoryJdbc.java
+++ src/main/java/com/lodestar/lodestar_server/career/repository/CareerRepositoryJdbc.java
@@ -1,6 +1,7 @@
 package com.lodestar.lodestar_server.career.repository;
 
 import com.lodestar.lodestar_server.career.dto.response.CareerDto;
+import com.lodestar.lodestar_server.career.entity.Career;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,19 +18,19 @@ import java.util.List;
 public class CareerRepositoryJdbc {
     private final JdbcTemplate jdbcTemplate;
 
-    public void saveCareers(Long userId, List<CareerDto> dtos) {
+    public void saveCareers(Long userId, List<Career> careers) {
 
         String saveCareerQuery = "insert into career(user_id, x, y1, y2, range_name, created_at, modified_at, status) VALUES (?,?,?,?,?,?,?,?)";
 
         jdbcTemplate.batchUpdate(saveCareerQuery, new BatchPreparedStatementSetter() {
             @Override
             public void setValues(PreparedStatement ps, int i) throws SQLException {
-                CareerDto dto = dtos.get(i);
+                Career career = careers.get(i);
                 ps.setLong(1, userId);
-                ps.setString(2, dto.getX());
-                ps.setLong(3, dto.getY().get(0));
-                ps.setLong(4, dto.getY().get(1));
-                ps.setString(5, dto.getRangeName());
+                ps.setString(2, career.getX());
+                ps.setLong(3, career.getY1());
+                ps.setLong(4, career.getY2());
+                ps.setString(5, career.getRangeName());
                 ps.setTimestamp(6, Timestamp.valueOf(LocalDateTime.now()));
                 ps.setTimestamp(7, Timestamp.valueOf(LocalDateTime.now()));
                 ps.setString(8,"y");
@@ -37,7 +38,7 @@ public class CareerRepositoryJdbc {
 
             @Override
             public int getBatchSize() {
-                return dtos.size();
+                return careers.size();
             }
         });
     }

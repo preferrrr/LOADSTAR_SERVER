dc8f51e259f833afb03de20d9a928cdd16a2c1bc
preferrrr <zzangsh2946@naver.com>
2024-02-19 21:02:35 +0900

    refactor : CreateCareerRequestDto -> SaveCareerRequestDto, toEntities 함수 추가

--- src/main/java/com/lodestar/lodestar_server/career/dto/request/SaveCareerRequestDto.java
+++ src/main/java/com/lodestar/lodestar_server/career/dto/request/SaveCareerRequestDto.java
@@ -1,20 +1,23 @@
 package com.lodestar.lodestar_server.career.dto.request;
 
 
+import com.lodestar.lodestar_server.career.entity.Career;
+import com.lodestar.lodestar_server.user.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
-public class CreateCareerRequestDto {
+public class SaveCareerRequestDto {
 
-    List<CareerDto> careers;
+    private List<CareerDto> careers;
 
     @Builder
-    private CreateCareerRequestDto(List<CareerDto> careers) {
+    private SaveCareerRequestDto(List<CareerDto> careers) {
         this.careers = careers;
     }
 
@@ -30,5 +33,11 @@ public class CreateCareerRequestDto {
             this.rangeName = rangeName;
         }
 
+    }
+
+    public List<Career> toEntities(User user) {
+        return careers.stream()
+                .map(careerDto -> Career.create(user, careerDto.x, careerDto.y.get(0), careerDto.y.get(1), careerDto.rangeName))
+                .collect(Collectors.toList());
     }
 }

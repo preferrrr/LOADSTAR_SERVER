37d8f7110b6db6d22522a3618fdcc827ddb69613
preferrrr <zzangsh2946@naver.com>
2024-02-20 19:44:56 +0900

    refactor : User 비즈니스와 서비스 분리

--- src/main/java/com/lodestar/lodestar_server/user/service/UserService.java
+++ src/main/java/com/lodestar/lodestar_server/user/service/UserService.java
@@ -4,36 +4,22 @@ import com.lodestar.lodestar_server.user.dto.request.ModifyPasswordRequestDto;
 import com.lodestar.lodestar_server.user.dto.request.LoginRequestDto;
 import com.lodestar.lodestar_server.user.dto.request.SignUpRequestDto;
 import com.lodestar.lodestar_server.user.dto.response.FindIdResponseDto;
-import com.lodestar.lodestar_server.user.dto.response.LoginResponseDto;
 import com.lodestar.lodestar_server.user.entity.User;
-import com.lodestar.lodestar_server.exception.*;
-import com.lodestar.lodestar_server.board.repository.BoardRepository;
-import com.lodestar.lodestar_server.comment.repository.CommentRepository;
-import com.lodestar.lodestar_server.user.respository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-@Slf4j
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository userRepository;
-    private final BoardRepository boardRepository;
-    private final CommentRepository commentRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final UserServiceSupport userServiceSupport;
 
+    @Transactional(readOnly = false)
     public void signUp(SignUpRequestDto signUpRequestDto) {
 
         //닉네임 중복확인, 이메일 인증 되었는지.
@@ -44,124 +30,59 @@ public class UserService {
         //5. 둘 다 true 되면 Join 활성화
         //6. Join 누르면 한 번 더 체크 해줄거냐? 안 해줄거냐 ?
 
-        if (duplicateUsername(signUpRequestDto.getUsername())) {
-            throw new DuplicateUsernameException(signUpRequestDto.getUsername());
-        }
-        if (duplicateEmail(signUpRequestDto.getEmail())) {
-            throw new DuplicateEmailException(signUpRequestDto.getEmail());
-        }
-        if (!signUpRequestDto.isEmailCheck() || !signUpRequestDto.isUsernameCheck()) {
-            throw new NotCheckException(signUpRequestDto.getEmail() + ", " + signUpRequestDto.getUsername());
-        }
+        userServiceSupport.checkExistsUsername(signUpRequestDto.getUsername());
 
-        String password = passwordEncoder.encode(signUpRequestDto.getPassword());
-        List<String> roles = new ArrayList<>();
-        roles.add("USER");
+        userServiceSupport.checkExistsEmail(signUpRequestDto.getEmail());
 
-        User user = User.builder()
-                .username(signUpRequestDto.getUsername())
-                .email(signUpRequestDto.getEmail())
-                .password(password)
-                .roles(roles)
-                .build();
+        String encryptedPassword = userServiceSupport.encryptPassword(signUpRequestDto.getPassword());
 
-        userRepository.save(user);
+        User user = User.create(signUpRequestDto.getUsername(), encryptedPassword, signUpRequestDto.getEmail(), List.of("USER"));
+
+        userServiceSupport.saveUser(user);
 
     }
 
 
-    public ResponseEntity<LoginResponseDto> login(HttpSession httpSession, LoginRequestDto loginRequestDto) {
+    @Transactional(readOnly = false)
+    public void login(HttpSession httpSession, LoginRequestDto loginRequestDto) {
 
-        if (!checkUsername(loginRequestDto.getUsername())
-                || !checkPassword(loginRequestDto.getUsername(), loginRequestDto.getPassword()))
-            throw new LoginFailException(loginRequestDto.getUsername() + ", " + loginRequestDto.getPassword());
+        User user = userServiceSupport.getUserByUsername(loginRequestDto.getUsername());
 
-        User user = userRepository.findByUsername(loginRequestDto.getUsername());
+        userServiceSupport.checkPasswordForLogin(user.getPassword(), loginRequestDto.getPassword());
 
-        List<Long> boardList = new ArrayList<>();
+        userServiceSupport.setSessionAttribute(httpSession, user);
 
-        httpSession.setAttribute("user", user);
-        httpSession.setAttribute("boards", boardList);
-
-        LoginResponseDto responseDto = LoginResponseDto.builder()
-                .userId(user.getId())
-                .build();
-
-        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
-    public void dupCheckUsername(String username) {
-        boolean result = checkUsername(username);
-        if (result) {
-            throw new ExistUsernameException(username);
-        }
-    }
 
-    public boolean checkUsername(String username) {
-        return userRepository.existsByUsername(username);
-    }
-
-    private boolean checkPassword(String username, String password) {
-        User checkUser = userRepository.findByUsername(username);
-        return passwordEncoder.matches(password, checkUser.getPassword());
-    }
-
-    private boolean duplicateUsername(String username) {
-        return userRepository.existsByUsername(username);
-    }
-
-    private boolean duplicateEmail(String email) {
-        return userRepository.existsByEmail(email);
+    public void checkExistsUsername(String username) {
+        userServiceSupport.checkExistsUsername(username);
     }
 
 
     public FindIdResponseDto findId(String email) {
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new NotExistEmailException(email));
 
-        FindIdResponseDto response = FindIdResponseDto.builder()
-                .message(encryptUsername(user.getUsername()))
-                .build();
+        User user = userServiceSupport.getUserByEmail(email);
 
-        return response;
+        return FindIdResponseDto.of(user.getUsername());
     }
 
+
+    @Transactional(readOnly = false)
     public void modifyPassword(User me, ModifyPasswordRequestDto requestDto) {
 
-        Optional<User> findUser = userRepository.findById(me.getId());
-        User user;
-        if (findUser.isPresent()) {
-            user = findUser.get();
-        } else {
-            throw new NotFoundException("[modify password] userId : " + me.getId());
-        }
+        User user = userServiceSupport.getUserById(me.getId());
 
-        if(!passwordEncoder.matches(requestDto.getCurrentPassword(), user.getPassword()))
-            throw new ModifyPwdFailException("userId : " + user.getId());
+        userServiceSupport.checkPasswordForLogin(user.getPassword(), requestDto.getCurrentPassword());
 
-        String modifyPassword = passwordEncoder.encode(requestDto.getModifyPassword());
+        String modifyPassword = userServiceSupport.encryptPassword(requestDto.getModifyPassword());
 
         user.modifyPassword(modifyPassword);
 
     }
 
 
-    private String encryptUsername(String username) {
-        int length = username.length();
-        int replaceLength = length / 3; // 문자열의 3분의 1 길이 계산
-
-        StringBuilder sb = new StringBuilder(username); // 변경 가능한 문자열로 변환
-
-        // 뒤에서 3분의 1 길이만큼 *로 대체
-        for (int i = Math.max(length - replaceLength, 0); i < length; i++) {
-            sb.setCharAt(i, '*');
-        }
-
-        String modifiedStr = sb.toString();
-
-        return modifiedStr;
-    }
-
-
+    @Transactional(readOnly = false)
     public void logout(HttpSession httpSession) {
         httpSession.invalidate();
     }
---
+++ src/main/java/com/lodestar/lodestar_server/user/service/UserServiceSupport.java
@@ -0,0 +1,66 @@
+package com.lodestar.lodestar_server.user.service;
+
+import com.lodestar.lodestar_server.user.exception.*;
+import com.lodestar.lodestar_server.user.entity.User;
+import com.lodestar.lodestar_server.user.respository.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class UserServiceSupport {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public String encryptPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    @Transactional(readOnly = false)
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void checkExistsUsername(String username) {
+        if (userRepository.existsByUsername(username)) {
+            throw new DuplicateUsernameException();
+        }
+    }
+
+    public void checkExistsEmail(String email) {
+        if (userRepository.existsByEmail(email))
+            throw new DuplicateEmailException();
+    }
+
+    public void checkPasswordForLogin(String realPassword, String requestPassword) {
+        if (passwordEncoder.matches(requestPassword, realPassword))
+            throw new NotMatchPasswordException();
+
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(UserNotFoundByUsernameException::new);
+    }
+
+    @Transactional(readOnly = false)
+    public void setSessionAttribute(HttpSession httpSession, User user) {
+        httpSession.setAttribute("user", user);
+        httpSession.setAttribute("boards", new ArrayList<Long>());
+
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(UserNotFoundByEmailException::new);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(UserNotFoundByIdException::new);
+    }
+}

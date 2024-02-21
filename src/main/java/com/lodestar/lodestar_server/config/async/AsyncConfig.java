f991b99c2af0ba141ca8d48e163fdbe93eafaffb
preferrrr <zzangsh2946@naver.com>
2024-02-21 22:27:58 +0900

    feat : 비동기 처리를 위한 AsyncConfig 추가

---
+++ src/main/java/com/lodestar/lodestar_server/config/async/AsyncConfig.java
@@ -0,0 +1,35 @@
+package com.lodestar.lodestar_server.config.async;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(10);
+        executor.setThreadNamePrefix("mail-send-");
+        executor.setQueueCapacity(50);
+        executor.setKeepAliveSeconds(30);
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return AsyncConfigurer.super.getAsyncUncaughtExceptionHandler();
+    }
+
+
+}

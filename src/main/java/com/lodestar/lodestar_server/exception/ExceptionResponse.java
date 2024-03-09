313a55f9e8cdf9fb44c7d28daddadd7daa0d8650
preferrrr <zzangsh2946@naver.com>
2024-03-09 19:59:54 +0900

    feat : ExceptionResponse 추가

---
+++ src/main/java/com/lodestar/lodestar_server/exception/ExceptionResponse.java
@@ -0,0 +1,39 @@
+package com.lodestar.lodestar_server.exception;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ExceptionResponse {
+
+    private HttpStatus status;
+    private String message;
+    private LocalDateTime time;
+
+    @Builder
+    private ExceptionResponse(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+        this.time = LocalDateTime.now();
+    }
+
+    public static ExceptionResponse of(ExceptionCode exceptionCode, String message) {
+        return ExceptionResponse.builder()
+                .status(exceptionCode.getHttpStatus())
+                .message(message)
+                .build();
+    }
+
+    public static ExceptionResponse of(ExceptionCode exceptionCode) {
+        return ExceptionResponse.builder()
+                .status(exceptionCode.getHttpStatus())
+                .message(exceptionCode.getMessage())
+                .build();
+    }
+}

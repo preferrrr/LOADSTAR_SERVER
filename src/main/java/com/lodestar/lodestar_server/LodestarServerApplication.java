c9d52cf4203ed6322d2a949a7ca7a4334e893196
preferrrr <zzangsh2946@naver.com>
2024-03-09 17:49:21 +0900

    refactor : JpaAuditing config 파일로 변경

--- src/main/java/com/lodestar/lodestar_server/LodestarServerApplication.java
+++ src/main/java/com/lodestar/lodestar_server/LodestarServerApplication.java
@@ -5,13 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class LodestarServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LodestarServerApplication.class, args);
-		//github_actions_test
 	}
 
 }
